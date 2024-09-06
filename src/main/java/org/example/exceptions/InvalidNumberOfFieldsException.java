b9352df28b53205364d6c51e7cfec9c0bff58fe7
Akos Bittman <a11911897@univie.ac.at>
2024-09-06 14:55:01 +0200

    Cleanup

--- src/main/java/org/example/exceptions/InvalidNumberOfFieldsException.java
+++ src/main/java/org/example/exceptions/InvalidNumberOfFieldsException.java
@@ -1,9 +1,6 @@
 package org.example.exceptions;
 
 public class InvalidNumberOfFieldsException extends RuntimeException{
-    //TODO throw nr of actual fields?
-
-
     public InvalidNumberOfFieldsException() {
         super("Number of fields must be >=2 AND <=50");
     }
